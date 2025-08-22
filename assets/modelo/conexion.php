ae9f8e3d6fd44f210f73bcf6627272878338a00c
CrisHDZ <leongar919@gmail.com>
2025-08-22 10:30:00 -0600

    V.2.0

--- assets/modelo/conexion.php
+++ assets/modelo/conexion.php
@@ -3,13 +3,13 @@
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-    /*$servername = "34.71.24.92";
+    $servername = "34.71.24.92";
     $username = "root";
     $password = "N8516uCCY2gsdfWW7TRa8AF0cnSI9U";
-    */
-    $servername = "localhost";
+    /*$servername = "localhost";
     $username = "root";
     $password = "1234";
+    */
 
     $database = "curso_mercadotecnia";
 
